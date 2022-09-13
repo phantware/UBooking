552730f4220e5dafee7ddfdff707d85cceb1a83a
phantware <phantmoney2011@gmail.com>
2022-09-13 08:07:19 +0100

    toggle added for open and close date

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,6 +15,7 @@ import 'react-date-range/dist/theme/default.css'
 import { format } from 'date-fns'
 
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false)
   const [date, setDate] = useState([
     {
       startDate: new Date(),
@@ -64,17 +65,22 @@ const Header = () => {
           </div>
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
-            <span className='headerSearchText'>{`${format(
-              date[0].startDate,
+            <span
+              onClick={() => setOpenDate(!openDate)}
+              className='headerSearchText'
+            >{`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(
+              date[0].endDate,
               'MM/dd/yyyy'
-            )} to ${format(date[0].endDate, 'MM/dd/yyyy')} `}</span>
-            <DateRange
-              editableDateInputs={true}
-              onChange={(item) => setDate([item.selection])}
-              moveRangeOnFirstSelection={false}
-              ranges={date}
-              className='date'
-            />
+            )} `}</span>
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className='date'
+              />
+            )}
           </div>
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
