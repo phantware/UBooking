7a2a387424dd5682ee100cbe79bdd858ffe539cc
phantware <phantmoney2011@gmail.com>
2022-09-13 07:53:14 +0100

    date-fns and react-date-range package added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,17 @@ import {
   faPerson,
 } from '@fortawesome/free-solid-svg-icons'
 import './header.css'
+import { DateRange } from 'react-date-range'
+import { useState } from 'react'
 
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection',
+    },
+  ])
   return (
     <div className='header'>
       <div className='headerContainer'>
@@ -53,6 +62,12 @@ const Header = () => {
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
             <span className='headerSearchText'>date to date</span>
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item) => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+            />
           </div>
           <div className='headerSearchItem'>
             <FontAwesomeIcon icon={faPerson} className='headerIcon' />
