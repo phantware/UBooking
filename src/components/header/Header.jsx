e64e2ad9b7d56ad0e34bacba6b1414f6f52f439d
phantware <phantmoney2011@gmail.com>
2022-09-13 07:42:27 +0100

    Header component created and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,67 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+  faCalendarDays,
+  faPerson,
+} from '@fortawesome/free-solid-svg-icons'
 import './header.css'
 
 const Header = () => {
   return (
     <div className='header'>
-      <div className='headerList'>
-        <div className='headerListItem'></div>
+      <div className='headerContainer'>
+        <div className='headerList'>
+          <div className='headerListItem active'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faCar} />
+            <span>Attractions</span>
+          </div>
+          <div className='headerListItem'>
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxi</span>
+          </div>
+        </div>
+        <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
+        <p className='headerDesc'>
+          Get rewarded for your travels - unlock instant savings of 10% or more
+          with a free Ubooking account
+        </p>
+        <button className='headerBtn'>Sign in / Register</button>
+        <div className='headerSearch'>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faBed} className='headerIcon' />
+            <input
+              type='text'
+              placeholder='Where are you going?'
+              className='headerSearchInput'
+            />
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+            <span className='headerSearchText'>date to date</span>
+          </div>
+          <div className='headerSearchItem'>
+            <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+            <span className='headerSearchText'>2 adults 2 children 1 room</span>
+          </div>
+          <div className='headerSearchItem'>
+            <button className='headerBtn'>Search</button>
+          </div>
+        </div>
       </div>
     </div>
   )
