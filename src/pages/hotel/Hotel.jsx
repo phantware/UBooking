e21eb28370b128a1721e231df7def06b5d939a46
phantware <phantmoney2011@gmail.com>
2022-09-19 13:18:07 +0100

    hotel component styled

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './hotel.css'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
