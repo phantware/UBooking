17505783a4f4bc83c90fffdeb619e9a1a575a932
phantware <phantmoney2011@gmail.com>
2022-10-25 21:12:04 +0100

    reserve component created

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,6 +1,8 @@
 import './hotel.css'
-import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+import MailList from '../../components/mailList/MailList'
+import Footer from '../../components/footer/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCircleArrowLeft,
@@ -9,37 +11,34 @@ import {
   faLocationDot,
 } from '@fortawesome/free-solid-svg-icons'
 import { useContext, useState } from 'react'
-import MailList from '../../components/mailList/MailList'
-import Footer from '../../components/footer/Footer'
 import useFetch from '../../hooks/useFetch'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { SearchContext } from '../../context/SearchContext'
+import { AuthContext } from '../../context/AuthContext'
+import Reserve from '../../components/reserve/Reserve'
 
 const Hotel = () => {
   const location = useLocation()
   const id = location.pathname.split('/')[2]
   const [slideNumber, setSlideNumber] = useState(0)
   const [open, setOpen] = useState(false)
-  // const photos = [
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1',
-  //   },
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1',
-  //   },
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1',
-  //   },
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1',
-  //   },
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1',
-  //   },
-  //   {
-  //     src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1',
-  //   },
-  // ]
+  const [openModal, setOpenModal] = useState(false)
+
+  const { data, loading, error } = useFetch(`/hotels/find/${id}`)
+  const { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const { dates, options } = useContext(SearchContext)
+
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime())
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY)
+    return diffDays
+  }
+
+  const days = dayDifference(dates[0].endDate, dates[0].startDate)
+
   const handleOpen = (i) => {
     setSlideNumber(i)
     setOpen(true)
@@ -57,24 +56,19 @@ const Hotel = () => {
     setSlideNumber(newSlideNumber)
   }
 
-  const { data, loading, reFetch } = useFetch(`/hotels/find/${id}`)
-  const { dates, options } = useContext(SearchContext)
-
-  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24
-  function dayDifference(date1, date2) {
-    const timeDiff = Math.abs(date2.getTime() - date1.getTime())
-    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY)
-    return diffDays
+  const handleClick = () => {
+    if (user) {
+      setOpenModal(true)
+    } else {
+      navigate('/login')
+    }
   }
-
-  const days = dayDifference(dates[0].endDate, dates[0].startDate)
-
   return (
     <div>
       <Navbar />
       <Header type='list' />
       {loading ? (
-        'Loading, please wait'
+        'loading'
       ) : (
         <div className='hotelContainer'>
           {open && (
@@ -91,7 +85,7 @@ const Hotel = () => {
               />
               <div className='sliderWrapper'>
                 <img
-                  src={data?.photos[slideNumber]}
+                  src={data.photos[slideNumber]}
                   alt=''
                   className='sliderImg'
                 />
@@ -144,7 +138,7 @@ const Hotel = () => {
                   <b>${days * data.cheapestPrice * options.room}</b> ({days}{' '}
                   nights)
                 </h2>
-                <button>Reserve or Book Now!</button>
+                <button onClick={handleClick}>Reserve or Book Now!</button>
               </div>
             </div>
           </div>
@@ -152,6 +146,7 @@ const Hotel = () => {
           <Footer />
         </div>
       )}
+      {openModal && <Reserve setOpen={setOpenModal} hotelId={id} />}
     </div>
   )
 }
